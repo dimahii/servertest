a5c192115e9c9bf904e068eedf9fb2756c7ede93
dimahii <dimahii@yahoo.com>
2019-05-02 04:57:53 +0430

    add update and delete at 12,2,1398

--- shop/mana/urls.py
+++ shop/mana/urls.py
@@ -4,5 +4,10 @@ from . import views
 app_name = 'mana'
 urlpatterns=[
     path('add/',views.add,name='add'),
-    path('accept/',views.accept,name='accept')
+    path('accept/',views.accept,name='accept'),
+    path('detail/<int:id>',views.detail,name='detail'),
+    path('',views.index,name='index'),
+    path('edit',views.edit,name='edit'),
+    path('delete/<int:id>',views.delete,name='delete'),
+    path('update/<int:id>',views.update,name='update')
 ]
--- shop/mana/views.py
+++ shop/mana/views.py
@@ -5,6 +5,12 @@ from django.utils import timezone
 from random import randint
 from . import date
 # Create your views here.
+def index(request):
+    customers = Customers
+    context={
+    'list' : customers,
+    }
+    return render(request,'customers/index.html',context=context)
 def add(request):
     list = Customers
     context={
@@ -12,7 +18,7 @@ def add(request):
     }
     return render(request,'customers/add.html',context=context)
 def accept(request):
-    list = Customers
+    customer_list = Customers
     try:
         description = request.POST['description']
         name = request.POST['name']
@@ -25,8 +31,76 @@ def accept(request):
         c = Customers(customer_name=name,description=description,customer_lastname=last_name,phone_number=phone_number,customer_id=customer_id)
         c.save()
         real_date=date.gregorian_to_jalali(timezone.now().year,timezone.now().month,timezone.now().day)
+        all = customer_list.objects.order_by('-service_date')[:15]
+
         context={
+            'all' : all,
             'customer' : c,
             'real_date' : real_date
         }
         return render(request,'customers/accept.html',context)
+def detail(request,id):
+    list = Customers
+    print(id)
+    c = list.objects.get(pk=id)
+    context={
+    'customer' : c
+    }
+    return render (request,'customers/detail.html',context)
+
+def edit(request):
+    customer_list = Customers
+    try:
+        search = request.POST['search']
+    except:
+        raise
+    else:
+        if (customer_list.objects.filter(customer_name__startswith = search)):
+            c = customer_list.objects.filter(customer_name__startswith = search)
+            context={
+            'list' : c
+            }
+            return render(request,'customers/edit.html',context)
+        elif (customer_list.objects.filter(customer_lastname__startswith = search)):
+            c =customer_list.objects.filter(customer_lastname__startswith = search)
+            context={
+            'list' : c
+            }
+            return render(request,'customers/edit.html',context)
+        elif (customer_list.objects.filter(customer_id__startswith = search)):
+            c = customer_list.objects.filter(customer_id__startswith = search)
+            context={
+            'list' : c
+            }
+            return render(request,'customers/edit.html',context)
+        elif (customer_list.objects.filter(phone_number__startswith = search)):
+            c = customer_list.objects.filter(phone_number__startswith = search)
+            context={
+            'list' : c
+            }
+            return render(request,'customers/edit.html',context)
+def delete(request,id):
+    customer_list = Customers
+    c = customer_list.objects.get(pk=id)
+    c.delete()
+    return HttpResponse('object is deleted')
+def update(request,id):
+    customer_list = Customers
+    c = customer_list.objects.get(pk=id)
+    try:
+        description = request.POST['description']
+        name = request.POST['name']
+        last_name = request.POST['last_name']
+        phone_number = request.POST['phone_number']
+    except:
+        raise
+    else:
+        c.description = description
+        c.save()
+        c.customer_name = name
+        c.save()
+        c.customer_lastname = last_name
+        c.save()
+        c.phone_number = phone_number
+        c.save()
+    return HttpResponse('object is updated')
